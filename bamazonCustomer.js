a887fffaf6ae79d72a87df3ce33eaeeed9bec028
Zachary Pleasant <zpleasant10@gmail.com>
2018-02-16 12:37:58 -0600

    Added flow chart/pseudocode for bamazonCustomer logic.

---
+++ bamazonCustomer.js
@@ -0,0 +1,13 @@
+// step 1: load npm packages and connect to database
+
+// step 2: display all the current products
+
+// stept 3: prompt user for id of product they would like to buy
+
+// step 4:prompt user for amount of the product they want to buy
+
+// step 5: take in both parameters (product id & amount), then check to see (if-else statement?) if they is a sufficient quantity
+    // if sufficient quantity
+        // update SQL database with current amount
+        // show total cost to customer for order
+    // else display "Insufficient Quantitity!"
