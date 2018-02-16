a83773d7694b9a80689935f9e8813c752dd67567
Zachary Pleasant <zpleasant10@gmail.com>
2018-02-15 21:01:23 -0600

    Populated schema file and added a seed file for inserting data.

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE database bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+  item_id INTEGER NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR(100) NULL,
+  price DECIMAL(15,2) NOT NULL,
+  stock_quantity INTEGER(10) NULL,
+  PRIMARY KEY (item_id)
+);
