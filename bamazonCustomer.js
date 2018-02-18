8a4bcd03856aab57390660f67e6535ece2cc1785
Zachary Pleasant <zpleasant10@gmail.com>
2018-02-18 17:15:23 -0600

    View Low Inventory function is working.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -27,11 +27,11 @@ function readProducts() {
     });
 }
 
-// step 3: prompt user for id of product they would like to buy and amount they want to buy. Then check the database against the amount and either run purchase or tell them insufficient quantity.
+// step 3: prompt user for id of product they would like to buy and amount they want to buy. Then check the database against the amount and either run purchase or tell them insufficient quantity
 function order() {
     inquirer.prompt([{
         name: "userChoice",
-        type: "input",
+        type: "list",
         message: "Welcome to Bamazon. Please make a selection:",
         choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]          
     },
@@ -44,7 +44,7 @@ function order() {
     ]).then(function (answer) {
         console.log(answer.userChoice);
         console.log(answer.amount);
-        connection.query(`SELECT product_name, price, stock_quantity FROM product WHERE item_id=${answer.userChoice}`, function (error, results){
+        connection.query(`SELECT product_name, price, stock_quantity FROM product WHERE item_id=${answer.userChoice}`, function (error, results) {
             if (error) throw error;
             else if (results[0].stock_quantity >= answer.amount) {
                 connection.query("UPDATE product SET stock_quantity=stock_quantity-? WHERE ?", [answer.amount,{item_id: answer.userChoice}],
--- bamazonManager.js
+++ bamazonManager.js
@@ -13,4 +13,57 @@ var connection = mysql.createConnection({
 connection.connect(function (error) {
     if (error) throw (error);
     // call the function that displays the manager options
-})
+    managerOptions();
+});
+
+function managerOptions() {
+    inquirer.prompt([{
+        name: "managerChoice",
+        type: "list",
+        message: "Welcome to the Bamazon Manager Interface. Please make a selection:",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product",]          
+    }
+    ]).then(function (answer) {
+        console.log(answer.managerChoice);
+        if (answer.managerChoice === "View Products for Sale") {
+            console.log("Viewing products for sale");
+            viewProducts();
+        } else if (answer.managerChoice === "View Low Inventory") {
+            console.log("Viewing low inventory");
+            // function to view low inventory
+            viewLowInventory();
+        } else if (answer.managerChoice === "Add to Inventory") {
+            console.log("Adding to inventory");
+            // function to add inventory
+        } else {
+            console.log("Adding new product");
+            // function to add new product
+        }
+    });
+};
+
+function viewProducts() {
+    connection.query("SELECT * FROM product", function (error, results){
+        if (error) throw error;
+        console.log(results);
+    })
+}
+
+function viewLowInventory() {
+    connection.query("SELECT product_name, stock_quantity FROM product WHERE stock_quantity < 5", function (error, results) {
+        if (error) throw (error);
+        else if (results[0] === undefined) {
+            console.log("Everything is stocked");
+        } else {
+            console.log(results);
+        }
+    })
+}
+
+function addInventory() {
+
+}
+
+function addProduct() {
+
+}
